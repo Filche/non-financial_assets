1d36da617dcf9162bc54c68784bd8a21c72aa362
Filche <filche_merigold@mail.ru>
2023-06-02 12:48:55 +1000

    third commit

--- src/main/java/vkr/chebanuk/diplom/config/MVCConfig.java
+++ src/main/java/vkr/chebanuk/diplom/config/MVCConfig.java
@@ -8,9 +8,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MVCConfig implements WebMvcConfigurer {
 
+    final String TEMPLATE_PREFIX = "/WEB-INF/views/";
+
     public void addViewControllers(ViewControllerRegistry registry) {
 
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                        "/images/**",
+                        "/css/**",
+                        "/js/**")
+                .addResourceLocations(
+                        TEMPLATE_PREFIX + "images/",
+                        TEMPLATE_PREFIX + "css/",
+                        TEMPLATE_PREFIX + "js/");
     }
 }
